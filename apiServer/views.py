a68953a7ba54496430c96f295907dc5afdbd2cf7
JenHsuan <ofalpha@gmail.com>
2020-05-12 21:59:30 +0800

    integrate django app with next.js

--- apiServer/views.py
+++ apiServer/views.py
@@ -1,6 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.conf import settings
+import os
+import logging
+
+index_file_path = os.path.join(settings.REACT_APP_DIR, 'out', 'index.html')
 
 # Create your views here.
 def index(request):
     return HttpResponse('Django API server')
+
+def react(request):
+    try:
+        with open(index_file_path) as f:
+            return HttpResponse(f.read())
+    except FileNotFoundError:
+        logging.exception('Production build of app not found')
