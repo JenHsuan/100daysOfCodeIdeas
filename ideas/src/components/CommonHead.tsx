52b6c24aaec5f2336435d84c4ae5f625f87458dd
JenHsuan <ofalpha@gmail.com>
2020-06-20 15:18:13 +0800

    modify the cover image and ga account

--- ideas/src/components/CommonHead.tsx
+++ ideas/src/components/CommonHead.tsx
@@ -9,7 +9,7 @@ const CommonHead = ({title, url, description, keywords}) => {
             <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
             <meta property="og:title" content={title} />
             <meta property="og:url" content={url}/>
-            <meta property="og:image" content="https://raw.githubusercontent.com/JenHsuan/ALayman/master/views/images/ideas-getstarted.png"/>
+            <meta property="og:image" content="https://raw.githubusercontent.com/JenHsuan/ALayman/master/views/images/ideas-getstarted-cover.png"/>
             <meta property="og:description" content={description}/>
             <meta name="keywords" content={keywords}/>
             <meta name="author" content="Jen-Hsuan Hsieh (Sean Hsieh)"/>
