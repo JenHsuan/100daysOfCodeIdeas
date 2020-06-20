dc5e9131af0d8a5c69a1b9705331ab4b8bbcbf7b
JenHsuan <ofalpha@gmail.com>
2020-06-20 10:40:24 +0800

    add the static folder

--- CodeIdeas/settings.py
+++ CodeIdeas/settings.py
@@ -191,7 +191,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/_next/'
-#STATIC_ROOT = os.path.join(REACT_APP_DIR, 'out')
+STATIC_ROOT = os.path.join(REACT_APP_DIR, 'out')
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(REACT_APP_DIR, 'out', '_next'),
