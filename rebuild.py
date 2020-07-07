cc2cf399ee86a369a3be322545d48cae4141adf8
JenHsuan <ofalpha@gmail.com>
2020-07-07 19:45:49 +0800

    test

--- rebuild.py
+++ rebuild.py
@@ -1,5 +1,8 @@
 from pynpm import NPMPackage
-from os import listdir
+from os import listdir, walk
+for i,j,y in walk('.'):
+    print(i) 
+
 pkg = NPMPackage('ideas/package.json')
 pkg.install()
 pkg.run_script('build')
