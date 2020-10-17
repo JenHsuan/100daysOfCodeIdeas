2f24a9be7cb7ea86af1900bb103ca07b8c9751ad
JenHsuan <ofalpha@gmail.com>
2020-10-17 11:46:10 +0800

    test

--- apiServer/views.py
+++ apiServer/views.py
@@ -87,10 +87,10 @@ class Download(APIView):
                 data = json.load(f)
                 if type_param == 'dmg':
                     file_name = "%s-%s.%s" % (downloads_file_base, data['version'], type_param)
-                    return redirect("%s%s" % (cdn_base_path, file_name))
+                    return HttpResponse("%s%s" % (cdn_base_path, file_name))
                 elif type_param == 'exe':
                     file_name = "%s %s.%s" % (downloads_file_base, data['version'], type_param)
-                    return redirect("%s%s" % (cdn_base_path, file_name))
+                    return HttpResponse("%s%s" % (cdn_base_path, file_name))
                 else:
                     return HttpResponse(
                         """
